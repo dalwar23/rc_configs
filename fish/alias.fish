42eaa226abe2a44ce941968a316db8ebaa9804aa
Dalwar Hossain <5631766+dalwar23@users.noreply.github.com>
2022-06-02 11:42:04 +0200

    Update alias.fish

--- fish/alias.fish
+++ fish/alias.fish
@@ -1,37 +1,16 @@
-# git
-abbr -a gs  git status -sb
-abbr -a ga  git add
-abbr -a gc  git commit
-abbr -a gcm git commit -m
-abbr -a gca git commit --amend
-abbr -a gcl git clone
-abbr -a gco git checkout
-abbr -a gp  git push
-abbr -a gpl git pull
-abbr -a gl  git l
-abbr -a gd  git diff
-abbr -a gds git diff --staged
-abbr -a gr  git rebase -i HEAD~15
-abbr -a gf  git fetch
-abbr -a gfc git findcommit
-abbr -a gfm git findmessage
-abbr -a gco git checkout
-
-# yadm
-abbr -a ys  yadm status -s -b
-abbr -a ya  yadm add
-abbr -a yc  yadm commit
-abbr -a ycm yadm commit -m
-abbr -a yp  yadm push
-abbr -a yl  yadm l
-abbr -a yd  yadm diff
-abbr -a yds yadm diff --staged
-
-# ls
-alias ll='ls -l'
-alias la='ls -a'
-alias lla='ls -la'
-alias lt='ls --tree'
+# Terraform aliases
+abbr -a tf terraform
+abbr -a tfi terraform init
+abbr -a tfp terraform plan
+abbr -a tfv terraform validate
+abbr -a tfl terraform fmt
+abbr -a tfa terraform apply
+abbr -a xtfa terraform apply -auto-approve
+abbr -a tfs terraform show
+abbr -a tfsl terraform state list
+abbr -a tfo terraform output
+abbr -a tfd terraform destroy
+abbr -a xtfd terraform destroy -auto-approve
 
 # misc
 alias reload='exec fish'
